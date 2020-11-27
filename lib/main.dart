54dab13b12591e4be5be01a4a4834fb23b3c973e
serpentarius <serpentarius.hollow@gmail.com>
2020-11-27 16:34:36 +0700

    feat: added MaterialApp title

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Flutter Camera Demo',
       home: Home(
         cameras: cameras,
       ),
